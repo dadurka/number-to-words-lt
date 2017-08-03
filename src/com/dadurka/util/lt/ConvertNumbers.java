b58987e431f5e102ee0b090f76f1383995c1451e
Virginijus Dadurkevičius <dadurka@fotonija.lt>
2017-08-03 13:17:37 +0300

    First release

--- src/com/dadurka/util/lt/ConvertNumbers.java
+++ src/com/dadurka/util/lt/ConvertNumbers.java
@@ -1,7 +1,7 @@
 package com.dadurka.util.lt;
 
 /*
- * Copyright (C) 2017 Virginijus Dadurkevičius (dadurka@gmail . com)
+ * Copyright (C) 2017 Virginijus Dadurkevičius (dadurka @ gmail . com)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
